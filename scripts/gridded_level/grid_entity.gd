63fb5729b9804938d46a8fde5b4127f5072b0fa3
Martin Zackrisson <martin.zackrisson@fripost.org>
2025-05-23 17:13:19 +0200

    Add input mapping for crawling

--- scripts/gridded_level/grid_entity.gd
+++ scripts/gridded_level/grid_entity.gd
@@ -4,24 +4,24 @@ class_name GridEntity
 var node: GridNode
 
 func _ready() -> void:
-    node = _find_node_parent(self)
+	node = _find_node_parent(self)
 
 func _find_node_parent(current: Node) ->  GridNode:
-    var parent: Node = current.get_parent()
+	var parent: Node = current.get_parent()
 
-    if parent == null:
-        push_warning("Entity %s not a child of a GridNode" % name)
-        return null
+	if parent == null:
+		push_warning("Entity %s not a child of a GridNode" % name)
+		return null
 
-    if parent is GridNode:
-        return parent as GridNode
+	if parent is GridNode:
+		return parent as GridNode
 
-    return _find_node_parent(parent)
+	return _find_node_parent(parent)
 
 func coordnates() -> Vector3i:
-    if node == null:
-        push_error("Entity %s isn't at a node, accessing its coordinates makes no sense" % name)
-        print_stack()
-        return Vector3i.ZERO
+	if node == null:
+		push_error("Entity %s isn't at a node, accessing its coordinates makes no sense" % name)
+		print_stack()
+		return Vector3i.ZERO
 
-    return node.coordinates
+	return node.coordinates
