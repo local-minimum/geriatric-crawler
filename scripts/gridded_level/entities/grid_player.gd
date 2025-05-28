7e8130b4dac065bce140c4da8083fe133eabd2c9
Martin Zackrisson <martin.zackrisson@fripost.org>
2025-05-28 11:59:58 +0200

    Repeat moves

--- scripts/gridded_level/entities/grid_player.gd
+++ scripts/gridded_level/entities/grid_player.gd
@@ -4,6 +4,9 @@ class_name GridPlayer
 @export
 var spawn_node: GridNode
 
+@export
+var persist_repeat_moves: bool
+
 func _ready() -> void:
     if spawn_node != null:
         var anchor: GridAnchor = spawn_node.get_anchor(down)
@@ -19,6 +22,8 @@ func _ready() -> void:
     # preset as a child of a node
     super()
 
+var _repeat_movement: Array[Movement.MovementType] = []
+
 func _input(event: InputEvent) -> void:
     if transportation_mode.mode == TransportationMode.NONE:
         return
@@ -27,18 +32,35 @@ func _input(event: InputEvent) -> void:
         if event.is_action_pressed("crawl_forward"):
             if !attempt_movement(Movement.MovementType.FORWARD):
                 print_debug("Refused Forward")
+            _held_movement(Movement.MovementType.FORWARD)
+        elif event.is_action_released("crawl_forward"):
+            _clear_held_movement(Movement.MovementType.FORWARD)
+
         elif event.is_action_pressed("crawl_backward"):
             if !attempt_movement(Movement.MovementType.BACK):
                 print_debug("Refused Backward")
+            _held_movement(Movement.MovementType.BACK)
+        elif event.is_action_released("crawl_backward"):
+            _clear_held_movement(Movement.MovementType.BACK)
+
         elif event.is_action_pressed("crawl_strafe_left"):
             if !attempt_movement(Movement.MovementType.STRAFE_LEFT):
                 print_debug("Refused Strafe Left")
+            _held_movement(Movement.MovementType.STRAFE_LEFT)
+        elif event.is_action_released("crawl_strafe_left"):
+            _clear_held_movement(Movement.MovementType.STRAFE_LEFT)
+
         elif event.is_action_pressed("crawl_strafe_right"):
             if !attempt_movement(Movement.MovementType.STRAFE_RIGHT):
                 print_debug("Refused Strafe Right")
+            _held_movement(Movement.MovementType.STRAFE_RIGHT)
+        elif event.is_action_released("crawl_strafe_right"):
+            _clear_held_movement(Movement.MovementType.STRAFE_RIGHT)
+
         elif event.is_action_pressed("crawl_turn_left"):
             if !attempt_movement(Movement.MovementType.TURN_COUNTER_CLOCKWISE):
                 print_debug("Refused Rotate Left")
+
         elif event.is_action_pressed("crawl_turn_right"):
             if !attempt_movement(Movement.MovementType.TURN_CLOCKWISE):
                 print_debug("Refused Rotate Right")
@@ -50,3 +72,22 @@ func _input(event: InputEvent) -> void:
             CardinalDirections.name(look_direction),
             CardinalDirections.name(down),
             transportation_mode.humanize()])
+
+func _held_movement(movement: Movement.MovementType) -> void:
+    if persist_repeat_moves:
+        if !_repeat_movement.has(movement):
+            _repeat_movement.append(movement)
+    else:
+        _repeat_movement[0] = movement
+
+func _clear_held_movement(movement: Movement.MovementType) -> void:
+    _repeat_movement.erase(movement)
+
+func _process(_delta: float) -> void:
+    if is_moving():
+        return
+
+    var count: int = _repeat_movement.size()
+    if count > 0:
+        if !attempt_movement(_repeat_movement[count - 1], false):
+            _clear_held_movement(_repeat_movement[count - 1])
