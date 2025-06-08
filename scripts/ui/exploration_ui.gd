3227a9fe8810045cc72c465170955452d2586ff7
Martin Zackrisson <martin.zackrisson@fripost.org>
2025-06-08 11:53:29 +0200

    Screen buttons

---
+++ scripts/ui/exploration_ui.gd
@@ -0,0 +1,32 @@
+extends Panel
+class_name  ExplorationUI
+
+@export
+var level: GridLevel
+
+
+func _on_forward_pressed() -> void:
+    if !level.player.attempt_movement(Movement.MovementType.FORWARD):
+        print_debug("Refused Forward")
+
+func _on_back_pressed() -> void:
+    if !level.player.attempt_movement(Movement.MovementType.BACK):
+        print_debug("Refused Back")
+
+func _on_turn_left_pressed() -> void:
+    if !level.player.attempt_movement(Movement.MovementType.TURN_COUNTER_CLOCKWISE):
+        print_debug("Refused Turn Left")
+
+func _on_turn_right_pressed() -> void:
+    if !level.player.attempt_movement(Movement.MovementType.TURN_CLOCKWISE):
+        print_debug("Refused Turn Right")
+
+
+func _on_strafe_left_pressed() -> void:
+    if !level.player.attempt_movement(Movement.MovementType.STRAFE_LEFT):
+        print_debug("Refused Strafe Left")
+
+
+func _on_strafe_right_pressed() -> void:
+    if !level.player.attempt_movement(Movement.MovementType.STRAFE_RIGHT):
+        print_debug("Refused Strafe Right")
