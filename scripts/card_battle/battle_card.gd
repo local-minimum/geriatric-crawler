c1fe369776ac7cec82e82d422779bb5774e6d709
Martin Zackrisson <martin.zackrisson@fripost.org>
2025-06-11 17:08:18 +0200

    Design basic card

---
+++ scripts/card_battle/battle_card.gd
@@ -0,0 +1,20 @@
+extends NinePatchRect
+class_name BattleCard
+
+@export
+var suiteIcon: TextureRect
+
+@export
+var rankLabel: Label
+
+@export
+var cardIcon: TextureRect
+
+@export
+var primaryEffect: RichTextLabel
+
+@export
+var divider: Control
+
+@export
+var secondaryEffect: RichTextLabel
