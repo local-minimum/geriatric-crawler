661f2efb12bd926cc77408279819eb0f9842e0b9
Martin Zackrisson <martin.zackrisson@fripost.org>
2025-07-16 10:13:21 +0200

    Remove old compass roll animation

--- scripts/ui/compass_ui.gd
+++ scripts/ui/compass_ui.gd
@@ -114,33 +114,35 @@ func _animate_roll_rotation(
 
     var left_coords: Vector2 = _get_coordinates(CompassCardinalLabelPosition.LEFT)
     var right_coords: Vector2 = _get_coordinates(CompassCardinalLabelPosition.RIGHT)
-    var up_coords: Vector2 = _get_coordinates(CompassCardinalLabelPosition.UP)
-    var down_coords: Vector2 = _get_coordinates(CompassCardinalLabelPosition.DOWN)
+    var left_up_coords: Vector2 = _get_coordinates(CompassCardinalLabelPosition.LEFT_UP)
+    var left_down_coords: Vector2 = _get_coordinates(CompassCardinalLabelPosition.LEFT_DOWN)
+    var right_up_coords: Vector2 = _get_coordinates(CompassCardinalLabelPosition.RIGHT_UP)
+    var right_down_coords: Vector2 = _get_coordinates(CompassCardinalLabelPosition.RIGHT_DOWN)
 
     @warning_ignore_start("return_value_discarded")
     if clockwise:
-        tween.tween_property(_get_cardinal(old_left), "global_position", up_coords, _animation_duration)
+        tween.tween_property(_get_cardinal(old_left), "global_position", left_up_coords, _animation_duration)
 
         var left_label: Control = _get_cardinal(left)
-        left_label.global_position = down_coords
+        left_label.global_position = left_down_coords
         tween.parallel().tween_property(left_label, "global_position", left_coords, _animation_duration)
 
-        tween.parallel().tween_property(_get_cardinal(old_right), "global_position", down_coords, _animation_duration)
+        tween.parallel().tween_property(_get_cardinal(old_right), "global_position", right_down_coords, _animation_duration)
 
         var right_label: Control = _get_cardinal(right)
-        right_label.global_position = up_coords
+        right_label.global_position = right_up_coords
         tween.parallel().tween_property(right_label, "global_position", right_coords, _animation_duration)
     else:
-        tween.tween_property(_get_cardinal(old_right), "global_position", up_coords, _animation_duration)
+        tween.tween_property(_get_cardinal(old_right), "global_position", right_up_coords, _animation_duration)
 
         var right_label: Control = _get_cardinal(right)
-        right_label.global_position = down_coords
+        right_label.global_position = right_down_coords
         tween.parallel().tween_property(right_label, "global_position", right_coords, _animation_duration)
 
-        tween.parallel().tween_property(_get_cardinal(old_left), "global_position", down_coords, _animation_duration)
+        tween.parallel().tween_property(_get_cardinal(old_left), "global_position", left_down_coords, _animation_duration)
 
         var left_label: Control = _get_cardinal(left)
-        left_label.global_position = up_coords
+        left_label.global_position = left_up_coords
         tween.parallel().tween_property(left_label, "global_position", left_coords, _animation_duration)
     @warning_ignore_restore("return_value_discarded")
 
@@ -216,7 +218,7 @@ func _get_cardinal(direction: CardinalDirections.CardinalDirection) -> Control:
 
     return null
 
-enum CompassCardinalLabelPosition {FAR_LEFT, LEFT, MID, RIGHT, FAR_RIGHT, UP, DOWN}
+enum CompassCardinalLabelPosition {FAR_LEFT, LEFT, MID, RIGHT, FAR_RIGHT, UP, DOWN, RIGHT_UP, RIGHT_DOWN, LEFT_UP, LEFT_DOWN}
 
 @export
 var _vertical_label_offset: float = -14
@@ -235,7 +237,11 @@ func _get_coordinates(label_position: CompassCardinalLabelPosition) -> Vector2:
     match label_position:
         CompassCardinalLabelPosition.MID: return center
         CompassCardinalLabelPosition.LEFT: return center + Vector2.LEFT * offset
+        CompassCardinalLabelPosition.LEFT_UP: return center + Vector2.LEFT * offset + Vector2.UP * offset
+        CompassCardinalLabelPosition.LEFT_DOWN: return center + Vector2.LEFT * offset + Vector2.DOWN * offset
         CompassCardinalLabelPosition.RIGHT: return center + Vector2.RIGHT * offset
+        CompassCardinalLabelPosition.RIGHT_UP: return center + Vector2.RIGHT * offset + Vector2.UP * offset
+        CompassCardinalLabelPosition.RIGHT_DOWN: return center + Vector2.RIGHT * offset + Vector2.DOWN * offset
         CompassCardinalLabelPosition.FAR_LEFT: return center + Vector2.LEFT * far_offset
         CompassCardinalLabelPosition.FAR_RIGHT: return center + Vector2.RIGHT * far_offset
         CompassCardinalLabelPosition.UP: return center + Vector2.UP * offset
