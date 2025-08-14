c64202f2d5353a1b39a949fd26a177987947abd6
Martin Zackrisson <martin.zackrisson@fripost.org>
2025-08-14 11:53:27 +0200

    Worm orients but head direction is bugged

--- scripts/ui/hacking_game_ui.gd
+++ scripts/ui/hacking_game_ui.gd
@@ -98,10 +98,16 @@ var most_recent_attempt: Container
 var worm_head_tex: Texture
 
 @export
+var worm_head_dead_tex: Texture
+
+@export
 var worm_straight_tex: Texture
 
 @export
 var worm_angled_tex: Texture
+
+@export
+var worm_tail_tex: Texture
 
 const DEPLOY_BOMB_TEXT: String = "Deploy Bomb"
 const CANCEL_BOMB_TEXT: String = "Abort Bomb Deployment"
@@ -150,21 +156,30 @@ func _unhandled_input(event: InputEvent) -> void:
             _sync_inventory_actions()
 
 const WORM_TICK_FREQ: int = 500
+const WORM_SPEEDUP: int = 10
+const WORM_MAX_SPEED: int = 80
+var worm_ticks: int
 
 func _process(_delta: float) -> void:
     if _worm_moving && Time.get_ticks_msec() > _worm_next_tick:
         var new_head: Vector2i = _worm[0] + _worming_direction
-        if !_lower_field_backgrounds.has(new_head):
+        if !_lower_field_backgrounds.has(new_head) || _worm.has(new_head):
             _kill_worm()
             return
 
         _move_worm_head(new_head)
-        _worm_next_tick = Time.get_ticks_msec() + WORM_TICK_FREQ
+        worm_ticks += 1
+        _worm_next_tick = Time.get_ticks_msec() + _calculate_worm_speed()
+
+func _calculate_worm_speed() -> int:
+    return maxi(WORM_MAX_SPEED, WORM_TICK_FREQ - worm_ticks * WORM_SPEEDUP)
 
 func _move_worm_head(coords: Vector2i) -> void:
     _worm.push_front(coords)
     while _worm.size() > _worm_size:
-        _lower_field_backgrounds[_worm[_worm.size() - 1]].texture = null
+        var t_rect: TextureRect = _lower_field_backgrounds[_worm[_worm.size() - 1]]
+        t_rect.texture = null
+        t_rect.rotation_degrees = 0
         _worm.pop_back()
 
     _draw_worm()
@@ -172,16 +187,18 @@ func _move_worm_head(coords: Vector2i) -> void:
 func _kill_worm() -> void:
     _worm_moving = false
     _worming = false
+    _worming_navigation_container.hide()
 
     for size: int in range(_worm.size(), 0, -1):
         if size > 0:
-            _lower_field_backgrounds[_worm[size - 1]].texture = null
+            var t_rect: TextureRect = _lower_field_backgrounds[_worm[size - 1]]
+            t_rect.texture = null
+            t_rect.rotation_degrees = 0
+
         _worm.pop_back()
         _draw_worm()
 
-        print_debug("Worm %s %s" % [size, _worm])
-
-        await get_tree().create_timer(WORM_TICK_FREQ / 1000.0).timeout
+        await get_tree().create_timer(_calculate_worm_speed() * 0.001).timeout
 
     _cancel_worm()
     print_debug("Worm dead")
@@ -704,6 +721,7 @@ func _ready_bombing() -> void:
     toggle_shift_buttons(true)
     toggle_word_controls(true)
     _deploy_worm_button.disabled = true
+    _attempt_button.disabled = true
 
 func _cancel_worm() -> void:
     _worming = false
@@ -722,6 +740,7 @@ func _ready_worm() -> void:
     toggle_shift_buttons(true)
     _deploy_worm_button.text = CANCEL_WORM_TEXT
     _deploy_bomb_button.disabled = true
+    _attempt_button.disabled = true
     _worming_navigation_container.show()
     _worming_direction = Vector2i.LEFT
     @warning_ignore_start("integer_division")
@@ -746,39 +765,99 @@ func _ready_worm() -> void:
     if _game.use_worm():
         _deploy_worm_button.disabled = true
         _worming_countdown.hide()
+        worm_ticks = 0
         _worm_moving = true
         _worm_next_tick = Time.get_ticks_msec()
     else:
         _cancel_worm()
 
 func _on_worm_up_pressed() -> void:
-    _worming_direction = Vector2i.UP
+    if _worm_moving && _worming_direction != Vector2i.DOWN:
+        _worming_direction = Vector2i.UP
 
 func _on_worm_left_pressed() -> void:
-    _worming_direction = Vector2i.LEFT
+    if _worm_moving && _worming_direction != Vector2i.RIGHT:
+        _worming_direction = Vector2i.LEFT
 
 func _on_worm_down_pressed() -> void:
-    _worming_direction = Vector2i.DOWN
+    if _worm_moving && _worming_direction != Vector2i.UP:
+        _worming_direction = Vector2i.DOWN
 
 func _on_worm_right_pressed() -> void:
-    _worming_direction = Vector2i.RIGHT
+    if _worm_moving && _worming_direction != Vector2i.LEFT:
+        _worming_direction = Vector2i.RIGHT
 
 func _draw_worm() -> void:
     var s: int = _worm.size()
+    var prev_coords: Vector2i = Vector2i.ZERO
     for idx: int in range(s):
         var coords: Vector2i = _worm[idx]
+        var d1: Vector2i = prev_coords - coords
+        var next_coords: Vector2i = _worm[idx + 1] if idx + 1 < s else coords - d1
+        var d2: Vector2i = coords - next_coords
+
         if !_lower_field_backgrounds.has(coords):
             print_debug("%s not in %s" % [coords, _lower_field_backgrounds.keys()])
             continue
 
+        var t_rect: TextureRect = _lower_field_backgrounds[coords]
+        t_rect.pivot_offset = t_rect.size * 0.5
+        t_rect.flip_h = false
+
         if idx == 0:
-            _lower_field_backgrounds[coords].texture = worm_head_tex
+            t_rect.texture = worm_head_tex if _worming else worm_head_dead_tex
+            t_rect.rotation_degrees = _delta_to_degrees(_worming_direction)
+        elif idx == s - 1:
+            t_rect.texture = worm_tail_tex
+            t_rect.rotation_degrees = _delta_to_degrees(d1)
+        elif d1 == d2:
+            t_rect.texture = worm_straight_tex
+            t_rect.rotation_degrees = _delta_to_degrees(d1)
         else:
-            # TODO Deal with rotations and such
-            # TODO: Add worm tail
-            _lower_field_backgrounds[coords].texture = worm_straight_tex
+            t_rect.texture = worm_angled_tex
+
+            if d1 == Vector2i.UP:
+                t_rect.rotation_degrees = 0
+                if d2 == Vector2i.RIGHT:
+                    t_rect.flip_h = true
+                else:
+                    t_rect.flip_h = false
+            elif d1 == Vector2i.RIGHT:
+                t_rect.rotation_degrees = 90
+                if d2 == Vector2i.DOWN:
+                    t_rect.flip_h = true
+                else:
+                    t_rect.flip_h = false
+            elif d1 == Vector2i.DOWN:
+                t_rect.rotation_degrees = 180
+                if d2 == Vector2i.LEFT:
+                    t_rect.flip_h = true
+                else:
+                    t_rect.flip_h = false
+            else:
+                t_rect.rotation_degrees = -90
+                if d2 == Vector2i.UP:
+                    t_rect.flip_h = true
+                else:
+                    t_rect.flip_h = false
+
+        print_debug("Worm %s is %s is rotated %s" % [idx, coords, t_rect.rotation_degrees])
+        prev_coords = coords
+
+func _delta_to_degrees(delta: Vector2i) -> float:
+    match delta:
+        Vector2i.LEFT: return 0
+        Vector2i.UP: return 90
+        Vector2i.RIGHT: return 180
+        Vector2i.DOWN: return -90
+        _:
+            push_warning("Unexpected delta of %s" % delta)
+            return 0
 
 func _clear_drawn_worm() -> void:
     for coords: Vector2i in _worm:
         if _lower_field_backgrounds.has(coords):
-            _lower_field_backgrounds[coords].texture = null
+            var t_rect: TextureRect = _lower_field_backgrounds[coords]
+            t_rect.texture = null
+            t_rect.rotation_degrees = 0
+            t_rect.flip_h = false
