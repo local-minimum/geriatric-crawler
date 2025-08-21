2b6d62d460575abb5155719c723b4a218ed5c27d
Martin Zackrisson <martin.zackrisson@fripost.org>
2025-08-21 12:51:22 +0200

    Improve animations and autoscroll to bottom

--- scripts/ui/chap_ui.gd
+++ scripts/ui/chap_ui.gd
@@ -3,6 +3,8 @@ class_name ChapUI
 
 enum MessageAnimation { NONE, PARAGRAPH, SENTENCE, WORD, CHARACTER }
 
+@export_multiline var start_message: String
+
 @export var title: Label
 @export var scroll_container: ScrollContainer
 @export var messages: VBoxContainer
@@ -15,12 +17,18 @@ enum MessageAnimation { NONE, PARAGRAPH, SENTENCE, WORD, CHARACTER }
 const _SPACERS: String = " -_:/[]().,;\\/?!*&"
 var _next_word: RegEx = RegEx.new()
 var _next_sentence: RegEx = RegEx.new()
+var _paragraph: RegEx = RegEx.new()
 
 func _ready() -> void:
     if _next_word.compile("[^ \n]*[ \n]?") != OK:
         push_error("Next word pattern didn't compile")
     if _next_sentence.compile(".*?[.!?]+([ \n]|$)") != OK:
         push_error("Next sentence pattern didn't compile")
+    if _paragraph.compile("\n*(.|\n)*?(\n\n|\\Z)") != OK:
+        push_error("Next paragraph pattern didn't compile")
+
+    await get_tree().create_timer(1).timeout
+    add_message(start_message)
 
 func add_message(message: String) -> void:
     var label: Label = _get_message_label()
@@ -36,7 +44,11 @@ func _animate_message(label: Label, message: String) -> void:
     var end: int = _find_message_part_end(message, start)
     while end < message.length():
         end = _find_message_part_end(message, start)
+
         label.visible_characters = end
+        if get_tree().create_timer(0.1).connect("timeout", _scroll_to_bottom) != OK:
+            pass
+
         var part: String = message.substr(start, end - start)
 
         var delay: float = (end - start) * letter_pause
@@ -44,10 +56,14 @@ func _animate_message(label: Label, message: String) -> void:
         if part.contains(".") || part.contains("?") || part.contains("!"):
             delay += sentence_pause
 
-        await get_tree().create_timer(delay).timeout
+        if end < message.length():
+            await get_tree().create_timer(delay).timeout
         start = end
 
     label.visible_characters = -1
+
+func _scroll_to_bottom() -> void:
+    scroll_container.set_deferred("scroll_vertical", scroll_container.get_v_scroll_bar().max_value)
 
 func _find_message_part_end(message: String, start: int) -> int:
     match animation:
@@ -57,19 +73,19 @@ func _find_message_part_end(message: String, start: int) -> int:
         MessageAnimation.WORD:
             var match: RegExMatch = _next_word.search(message, start)
             if match:
-                return match.get_end() + 1
+                return match.get_end()
             return message.length()
 
         MessageAnimation.SENTENCE:
             var match: RegExMatch = _next_sentence.search(message, start)
             if match:
-                return match.get_end() + 1
+                return match.get_end()
             return message.length()
 
         MessageAnimation.PARAGRAPH:
-            var end: int = message.find("\n", start)
-            if end > start:
-                return end
+            var match: RegExMatch = _paragraph.search(message, start)
+            if match:
+                return match.get_start(2)
             return message.length()
 
     return message.length()
