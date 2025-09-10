85b198fa95b778ad919a92e114996ab91d2ecd14
Martin Zackrisson <martin.zackrisson@fripost.org>
2025-09-10 21:40:47 +0200

    fix ease

--- scripts/exploration_scene.gd
+++ scripts/exploration_scene.gd
@@ -36,15 +36,18 @@ func _ready() -> void:
     _disable_robot_death_effect()
 
 func _disable_robot_death_effect() -> void:
-    subviewport.set_instance_shader_parameter("pixels", -1)
+    var shader: ShaderMaterial = subviewport.material
+    shader.set_shader_parameter("pixels", -1)
+    # subviewport.set_instance_shader_parameter("pixels", -1)
     print_debug("[Exploration Scene] Disabled pixelating effect")
 
 func _handle_robot_death(_robot: Robot) -> void:
+    var shader: ShaderMaterial = subviewport.material
     print_debug("[Exploration Scene] Produce signal loss pixelating effect")
-    subviewport.set_instance_shader_parameter("pixels", 200)
-    subviewport.set_instance_shader_parameter("max_lerp", 0.1)
+    shader.set_shader_parameter("pixels", 200)
+    shader.set_shader_parameter("max_lerp", 0.1)
 
-    var duration: float = 1.0
+    var duration: float = 2.0
 
     var tween: Tween = create_tween()
     @warning_ignore_start("return_value_discarded")
@@ -52,7 +55,7 @@ func _handle_robot_death(_robot: Robot) -> void:
 
     tween.tween_method(
         func (progress: float) -> void:
-            subviewport.set_instance_shader_parameter("max_lerp", progress)
+            shader.set_shader_parameter("max_lerp", progress)
             ,
         0.1,
         0.8,
@@ -60,11 +63,20 @@ func _handle_robot_death(_robot: Robot) -> void:
     )
 
     tween.tween_method(
+        func (progress: float) -> void:
+            shader.set_shader_parameter("min_lerp", progress)
+            ,
+        0.0,
+        0.6,
+        duration
+    )
+
+    tween.tween_method(
         func (progress: int) -> void:
-            subviewport.set_instance_shader_parameter("pixels", progress)
+            shader.set_shader_parameter("pixels", progress)
             ,
         100,
-        8,
+        5,
         duration
     ).set_trans(Tween.TRANS_QUART).set_ease(Tween.EASE_OUT)
     @warning_ignore_restore("return_value_discarded")
