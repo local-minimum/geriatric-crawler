e850db1295bfaddc4d9d1cb709b949c9207ebe69
Martin Zackrisson <martin.zackrisson@fripost.org>
2025-10-23 18:51:32 +0200

    Work on global state saver

--- scripts/savesystem/extensions/global_game_state_saver.gd
+++ scripts/savesystem/extensions/global_game_state_saver.gd
@@ -1,43 +1,31 @@
-extends SaveExtension
+extends GlobalGameStateCoreSaver
 class_name GlobalGameStateSaver
 
-const _CREDITS_KEY: String = "credits"
 const _LOANS_KEY: String = "loans"
 
 const _INTEREST_RATE_KEY: String = "interest-rate"
 
 const _RENT_KEY: String = "rent"
 
-const _GAME_DAY_KEY: String = "day"
-
-@export var _save_key: String = "globals"
-
-func get_key() -> String:
-    return _save_key
 
 func load_from_initial_if_save_missing() -> bool:
     return false
 
 func retrieve_data(_extentsion_save_data: Dictionary) -> Dictionary:
-    return {
+    return super.retrieve_data(_extentsion_save_data).merged({
         _INTEREST_RATE_KEY: __GlobalGameState._interest_rate_points,
-        _CREDITS_KEY: __GlobalGameState._credits,
         _LOANS_KEY: __GlobalGameState._loans,
         _RENT_KEY: __GlobalGameState._rent,
-        _GAME_DAY_KEY: __GlobalGameState.game_day
-    }
-
-func initial_data(_extentsion_save_data: Dictionary) -> Dictionary:
-    return {}
+    }, true)
 
 func load_from_data(extentsion_save_data: Dictionary) -> void:
-    var credits: int = DictionaryUtils.safe_geti(extentsion_save_data, _CREDITS_KEY, 0, false)
+    super.load_from_data(extentsion_save_data)
+
     var loans: int = DictionaryUtils.safe_geti(extentsion_save_data, _LOANS_KEY, 0, false)
-    __GlobalGameState.set_credits(credits)
     __GlobalGameState.set_loans(loans)
 
     var interest_rate: int = DictionaryUtils.safe_geti(extentsion_save_data, _INTEREST_RATE_KEY, GlobalGameState.BASE_INTEREST_RATE, false)
     __GlobalGameState.set_interest_rate(interest_rate)
 
-    var game_day: int = DictionaryUtils.safe_geti(extentsion_save_data, _GAME_DAY_KEY, 0, false)
-    __GlobalGameState.set_game_day(game_day)
+    var rent: int = DictionaryUtils.safe_geti(extentsion_save_data, _RENT_KEY, 0, false)
+    __GlobalGameState.set_rent(rent)
